e1f3ff72d6d8ac365b5406d296b369214c9c169a
Juan Manuel Bolivar Mojica <jbolivarm2@ucentral.edu.co>
2026-03-24 19:12:58 -0500

    Create Reposotory

---
+++ src/main/java/com/h2/database/task/repository/TaskRepository.java
@@ -0,0 +1,11 @@
+package com.h2.database.task.repository;
+
+
+import com.h2.database.task.persistence.entity.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+
+}
