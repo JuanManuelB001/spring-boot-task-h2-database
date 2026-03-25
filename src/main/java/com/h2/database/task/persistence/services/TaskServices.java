7127acb101196e9f690a6afd7a5d47c8f8312dea
Juan Manuel Bolivar Mojica <jbolivarm2@ucentral.edu.co>
2026-03-24 19:17:25 -0500

    Create TaskService

--- src/main/java/com/h2/database/task/persistence/services/TaskServices.java
+++ src/main/java/com/h2/database/task/persistence/services/TaskServices.java
@@ -1,4 +1,31 @@
 package com.h2.database.task.persistence.services;
 
+
+import com.h2.database.task.persistence.entity.Task;
+import com.h2.database.task.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class TaskServices {
+    @Autowired
+    private TaskRepository repository;
+
+    // LIST ALL TASKS
+    public List<Task> getTasksAll(){
+        return  repository.findAll();
+    }
+
+    // CREATE NEW TASK
+    public Task createTask(Task task){
+        if(task.getNombre() == null || task.getNombre().isEmpty()
+        || task.getAreaTrabajo() == null || task.getAreaTrabajo().isEmpty()
+        ){
+            throw  new RuntimeException("Input All Values...");
+        }
+        return repository.save(task);
+    }
+
 }
